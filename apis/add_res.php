101c745ab1d7b9952e79d93fe65913b39ad3d3c0
vladimir <vladimirmawla2@gmail.com>
2022-05-28 22:47:06 +0300

    Adding add restaurants api

---
+++ apis/add_res.php
@@ -0,0 +1,22 @@
+<?php
+
+include("connection.php");
+
+
+
+$name = $_POST["name"];
+$location = $_POST["location"];
+$ratings = $_POST["ratings"];
+$img = $_POST["img"];
+//$data = base64_encode($img);
+
+$query = $mysqli->prepare("INSERT INTO restaurants(name, location, ratings, img) VALUES (?, ?, ?, ?)");
+$query->bind_param("ssis", $name, $location, $ratings, $img);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+
+?>
